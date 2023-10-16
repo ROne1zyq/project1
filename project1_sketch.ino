3f97d9a42242d64a0eb38f54c0cfd3381032768a
rickzhao <40881706+ROne1zyq@users.noreply.github.com>
2023-10-16 20:29:59 +0800

    Add files via upload

---
+++ project1_sketch.ino
@@ -0,0 +1,32 @@
+#define M_left1 10
+#define M_left2 11
+#define M_right1 6
+#define M_right2 7
+
+void setup() {
+  pinMode(M_left1, OUTPUT);
+  pinMode(M_left2, OUTPUT);
+  pinMode(M_right1, OUTPUT);
+  pinMode(M_right2, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+double USRead(int trig,int echo){
+  pinMode(trig,OUTPUT);
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  pinMode(echo,INPUT);
+  double t=pulseIn(echo,HIGH);
+  double distance=t*0.0344/2;
+  return distance;
+}
+
+void loop() {
+  if(USRead(12,13)<50){
+
+}
